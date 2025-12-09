1143906223851b2aea06fbe118b48234388697f5
Julio Rios <julio.rios.araujo@gmail.com>
2025-12-09 13:33:23 -0300

    Remove duplicate service folders from api-principal

--- api-principal/notification-service/src/domain/ports/IAlertRepository.ts
+++
@@ -1,9 +0,0 @@
-import { Alert } from '../entities/Alert';
-
-export interface IAlertRepository {
-  save(alert: Alert): Promise<Alert>;
-  findById(id: string): Promise<Alert | null>;
-  findAll(): Promise<Alert[]>;
-  findBySensorId(sensorId: string): Promise<Alert[]>;
-  findRecent(limit: number): Promise<Alert[]>;
-}
--- api-principal/sensor-service/src/domain/ports/ISensorRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Sensor } from '../entities/Sensor';
-
-export interface ISensorRepository {
-  findById(id: string): Promise<Sensor | null>;
-  findByActive(isActive: boolean): Promise<Sensor[]>;
-}
